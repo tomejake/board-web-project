466ae1a035ca3345d5bd2a3fc3d16a9cbdebeb30
Rhee MinJae <janus629@gmail.com>
2022-01-12 11:26:01 +0900

    Design(Layout) : 프로젝트 레이아웃
    프로젝트에 사용할 레이아웃입니다.
    src/components 폴더에 Header, Nav, Contents, Footer 파일을 구성하였고
    정적인 구성 요소(로고, 버튼, 푸터)는 구조 위치를 잡기 위해 임시로 넣었습니다.
    콘텐츠 영역은 영역 사이즈 확인을 위하여 HelloWorld의 기본 디폴트 값을
    사용하여 임시로 채웠습니다.
    
    Resolves: #3

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,67 +1,37 @@
 <template>
-  <body>
-    <div id="wrap">
-          <div id="header"></div>
-          <div id="nav"></div>
-          <div id="left"></div>
-          <div id="contents"></div>
-          <div id="right"></div>
-          <div id="footer"></div>
-      </div>
-  </body>
+  <div id="app">
+    <Header />
+    <Nav />
+    <Contents />
+    <Footer />
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+import Header from './components/Header.vue';
+import Nav from './components/Nav.vue';
+import Contents from './components/Contents.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Nav,
+    Contents,
+    Footer
+  }
+}
+</script>
+
+<style>
+html,body{padding: 0; margin: 0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0; padding: 0;
 }
- * {
-        margin: 0; padding: 0;
-    }
-    body {
-        background-color: #fff;
-    }
-    #wrap {
-        width: 500px;
-        margin: 0 auto;
-    }
-    #header {
-        width: 500px;
-        height: 50px;
-        background-color: #222;
-    }
-    #nav {
-        width: 500px;
-        height: 30px;
-        background-color: #333;
-    }
-    #left {
-        width: 50px;
-        height: 300px;
-        background-color: #777;
-        float: left;
-    }
-    #contents {
-        width: 400px;
-        height: 300px;
-        background-color: #555;
-        float: left;
-    }
-    #right {
-        width: 50px;
-        height: 300px;
-        background-color: #777;
-        float: left;
-    }
-    #footer {
-        width: 500px;
-        height: 50px;
-        background-color: #666;
-        clear: both;
-    }
 </style>
