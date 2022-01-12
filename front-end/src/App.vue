141e106167cd169fbb2d3836765487ddda6f9416
Rhee MinJae <janus629@gmail.com>
2022-01-12 10:08:36 +0900

    Design(Layout Prototype) : 프로토타입
    레이아웃 디자인 프로토타입 입니다.
    
    Resolves: #3

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <body>
+    <div id="wrap">
+          <div id="header"></div>
+          <div id="nav"></div>
+          <div id="left"></div>
+          <div id="contents"></div>
+          <div id="right"></div>
+          <div id="footer"></div>
+      </div>
+  </body>
 </template>
 
 <style lang="scss">
@@ -13,18 +18,50 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+ * {
+        margin: 0; padding: 0;
     }
-  }
-}
+    body {
+        background-color: #fff;
+    }
+    #wrap {
+        width: 500px;
+        margin: 0 auto;
+    }
+    #header {
+        width: 500px;
+        height: 50px;
+        background-color: #222;
+    }
+    #nav {
+        width: 500px;
+        height: 30px;
+        background-color: #333;
+    }
+    #left {
+        width: 50px;
+        height: 300px;
+        background-color: #777;
+        float: left;
+    }
+    #contents {
+        width: 400px;
+        height: 300px;
+        background-color: #555;
+        float: left;
+    }
+    #right {
+        width: 50px;
+        height: 300px;
+        background-color: #777;
+        float: left;
+    }
+    #footer {
+        width: 500px;
+        height: 50px;
+        background-color: #666;
+        clear: both;
+    }
 </style>
