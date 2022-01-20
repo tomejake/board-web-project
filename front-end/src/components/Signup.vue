52ddbd4c092934e4e375532c5b127f9e3e80519f
tomejake <tomejake22@gmail.com>
2022-01-20 09:46:10 +0900

    FEAT : proxy설정과 Signup.vue 코드정리
    - CORS문제 해결을 위해 fonrt-end에 vue.config.js파일 생성 후, port번호와 proxy등 설정
    - Signup.vue 잘못 사용되던 코드 정리 후, submitForm메소드 동작을 확인함
    
     Resolves: #31

--- front-end/src/components/Signup.vue
+++ front-end/src/components/Signup.vue
@@ -1,53 +1,59 @@
 <template>
-<form name="signUp">
+<form name="signUp" class="frm" @submit.prevent="submitForm">
   <h1>회원가입</h1>
-  <label> 아이디
-    <input class="signup-input" type="id"  id="userId" placeholder="Enter User ID"/>
-  </label>
-  <label> 비밀번호
-    <input class="signup-input" type="password" id="userPassword" placeholder="Enter User Password"/>
-  </label>
-  <label> 비밀번호 확인
-    <input class="signup-input" type="password" id="checkPassword" placeholder="Enter Password again">
-  </label>
-  <input type="submit" name="submit" id="submit" value="확인" />
+  <label for="userid"> 아이디</label>
+    <input class="signup-input" type="text" name="userid" id="userid" v-model="userid" placeholder="Enter User ID"/>
+  <label for="password">비밀번호 </label>
+    <input class="signup-input" type="password" name="password" id="password" v-model="password" placeholder="Enter User Password"/>
+  <label for="checkPassword">비밀번호 확인 </label> 
+    <input class="signup-input" type="password" name="checkPassword" id="checkPassword" placeholder="Enter Password again">
+  <input type="submit" id="submit" value="확 인" />
 </form>
 </template>
 
 <script>
 export default {
-  name : 'Signup'
+  name : 'Signup',
+  data() {
+    return {
+      userid: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm(){
+      console.log('123');
+    }
+  },
 }
 </script>
 
 <style>
 
-* {margin : 0; padding : 0;}
-
-form {
+.frm {
     width : 300px; margin : 50px auto ; border : 1px solid #ccc;
 }
 
-h1 {
+.frm h1 {
     display : block; font-size : 30px; text-align: center; margin : 15px 0;
 }
 
-label {
+.frm label {
     font-size : 20px; margin : 1px 0; padding: 0 10px; 
     color : #555; text-align: left;
 }
 
-.signup-input {
+.frm .signup-input {
     width : 100%; height : 35px; padding : 0 15px; 
     box-sizing : border-box; border : 1px solid #ddd;
 }
 
-#submit {
+.frm #submit {
     margin-top : 20px; display : block; width: 100%; height : 35px;
-    background : #444; color : white; border : none; cursor : pointer;
+    background : #444; color : white; border : none; cursor: pointer;
 }
 
-#submit:hover {
+.frm #submit:hover {
     background: #999;
 }
 </style>
--- src/main/java/com/gram/controller/BoardController.java
+++ src/main/java/com/gram/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.gram.controller;
 
 import com.gram.domain.Board;
+import com.gram.domain.User;
 import com.gram.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,7 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:8081" )
+@CrossOrigin(origins = "http://localhost:4000" )
 @RestController
 @RequestMapping("/api")
 public class BoardController {
@@ -17,14 +18,20 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping({"/write"})
+    @GetMapping("/write")
     public String board(@RequestParam(value="idx", defaultValue = "0") Long idx, Model model){
         model.addAttribute("board", boardService.findBoardByIdx(idx));
         return "/board/form";
     }
 
-    @GetMapping({"/list"})
+    @GetMapping("/list")
     public Page<Board> list(@PageableDefault(size = 10) Pageable pageable, Model model){
         return boardService.findBoardList(pageable);
+    }
+
+    @PostMapping("join")
+    @ResponseBody
+    public void Join(@RequestBody User user){
+        System.out.println("user = " + user.getUserid());
     }
 }
