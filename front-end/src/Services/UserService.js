f81e0212a016905a2736b275118b5eefc60e0032
tomejake <tomejake22@gmail.com>
2022-01-21 11:08:12 +0900

    FEAT : 로그인 기능
    - 로그인 form에서 서버로 값을 전달하면 /login에서 받은 데이터를
     UserService.login에서 확인 후 boolean값으로 반환
    - boolean 값 확인 후 if문으로 vue화면 조정
     Resolves: #30

--- front-end/src/Services/UserService.js
+++ front-end/src/Services/UserService.js
@@ -10,10 +10,6 @@ class UserService {
     loginUser(userData) {
         return axios.post(API_BASE_URL+'login', userData);
     }
-
-    getLoginResult(){
-        return axios.get(API_BASE_URL+'loginResult');
-    }
 }
 
 export default new UserService();
